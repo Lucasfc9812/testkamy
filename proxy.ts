503df664f4c1e94a37f0d1e3d4f9697d9e9ace44
Lucasfc9812 <120746112+Lucasfc9812@users.noreply.github.com>
2025-11-20 03:55:09 -0300

    Delete proxy.ts

--- proxy.ts
+++
@@ -1,9 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    proxyPrefetch: true, // substitui middlewarePrefetch
-  },
-};
-
-module.exports = nextConfig;
