d40a2ba8595e522f20fd34a05ecd344b75cc7614
Lucasfc9812 <120746112+Lucasfc9812@users.noreply.github.com>
2025-11-20 03:48:54 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
   experimental: {
-    middlewarePrefetch: 'flexible'
-  }
+    proxyPrefetch: true, // substitui middlewarePrefetch
+  },
 };
 
 module.exports = nextConfig;
